62db03e1e96516d077238cf5cffe1ec62273b6ae
paulvasquez12 <fpaulvasquez0302@gmail.com>
2025-10-14 18:37:38 -0500

    el ingeniero dijo que borre

--- frutas.py
+++ frutas.py
@@ -1,12 +1,1 @@
-canasta = ["pera","manzana","uva","naranja","bananana"]
-persona =(input("ingrese una fruta "))
 
-
-for fruta in canasta:
-    if fruta==persona:
-        print("si esta en la lista" )
-    else:
-        print("no esta en la lista ")
-ciclo = ["diseño multimedia","base de datos ","programacion orientada a objetos","matetaticas","metodologia de desarrolo sw sofware"]        
-notas = [9 , 10 , 8 , 8 , 9] 
-docentes = ["israel z", "cecilia n", ]  
